e53063ed7188156719aa1bc22bf4e77c018d320e
lintao_alex <lintao_alex@163.com>
2019-06-14 18:33:53 +0800

    adjust interface

--- src/Interface.ts
+++ src/Interface.ts
@@ -2,15 +2,16 @@
  * Created by lintao_alex on 2019/2/27
  */
 namespace Dream.common {
+    //用于定义可回收利用的对象
     export interface IClear {
         clear(): void;
     }
 
-    export interface IDispose extends IClear {
+    export interface IDispose {
         dispose(): void;
     }
 
-    export interface IClassIdentify<T> {
+    export interface IClass<T> {
         new(...args: any[]): T;
     }
 
